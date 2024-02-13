316a5adc2b05ae66a50b7c881a37f6603c30ef3f
Andre Silva <andreswebs@pm.me>
2024-02-12 23:03:25 -0300

    script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.config/nix-darwin
-darwin-rebuild build --flake .
-
+darwin-rebuild switch --flake .
